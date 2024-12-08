0df86de1875a0d844428eaaddd0f40d12bd807ed
GabryOsas <giggigiochicollaboro@gmail.com>
2024-12-08 19:23:52 +0100

    add comments

--- bubble.cpp
+++ bubble.cpp
@@ -3,24 +3,32 @@
 using namespace std;
 
 int main() {
+    //dichiarazione variabile temp e vettore vector (lunghezza 5)
     int temp;
     int vector[5];
+
+    //aggiungo gli elementi chiesti nel vettore
     for (int i = 0; i < 5; i++) {
         cout << "Inserisci l'elemento " << i << " della lista" << endl;
         cin >> vector[i];
     }
-    for (int i = 0; i < 5; i++) {
-        for (int j = i + 1; j < 5; j++){
-            if (vector[i] > vector[j]) {
-                temp = vector[j];
+
+    //ordino il vettore
+    for (int i = 0; i < 5; i++) { //seleziono il numero alla posizione i del vettore
+        for (int j = i + 1; j < 5; j++){ //seleziono il numero successivo 
+            if (vector[i] > vector[j]) { //controllo se vector[i] è maggiore di vector[j]
+                //risposta affermativa sostituisco
+                temp = vector[j]; 
                 vector[j] = vector[i];
                 vector[i] = temp;
             }
         }
     }
+
+    //restituisco il vettore ordinato
     cout << "Lista ordinata ottenuta: ";
     for (int i = 0; i < 5; i++) {
         cout << vector[i] << ", ";
     }   
-    return 0;
+    return 0; //fine
 }
